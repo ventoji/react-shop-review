5aff753df647f190aa1c2d4f573ffb4430e8ed99
Jorge Isaac Vento Maldonado <39380157+ventoji@users.noreply.github.com>
2020-08-20 00:09:43 +0200

    Including react router dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./styles.css";
 import Homepage from "./pages/homepage/homepage";
+import { Route, Switch } from "react-router-dom";
+// import RouterEx from "./RouterEx";
+
+function HatsPage() {
+  return <h1> hats page </h1>;
+}
 
 export default function App() {
   return (
     <div className="App">
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
