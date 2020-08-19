551ef2488d54f8fa0b64cb3906e57ef5bd2a4d3c
Jorge Isaac Vento Maldonado <39380157+ventoji@users.noreply.github.com>
2020-08-19 18:12:34 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>React shop</h1>
+      <h2>React 2020 course</h2>
+    </div>
+  );
+}
