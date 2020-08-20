f9b449eda8d7f865d0e396ca9f93950f4be88e8b
Jorge Isaac Vento Maldonado <39380157+ventoji@users.noreply.github.com>
2020-08-20 09:10:29 +0200

    Building shop page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import "./styles.css";
-import Homepage from "./pages/homepage/homepage";
+import HomePage from "./pages/homepage/homepage";
 import { Route, Switch } from "react-router-dom";
 // import RouterEx from "./RouterEx";
+import ShopPage from "./pages/shop/shop";
 
-function HatsPage() {
+/* function HatsPage() {
   return <h1> hats page </h1>;
-}
+} */
 
 export default function App() {
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/hats" component={HatsPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
