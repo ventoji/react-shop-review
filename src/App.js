c0e3e5c22090b2ddd263a28dff003b95cede1e4f
Jorge Isaac Vento Maldonado <39380157+ventoji@users.noreply.github.com>
2020-08-20 09:37:34 +0200

    Building Header Component Adding navigation and logo in svg format.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homepage/homepage";
 import { Route, Switch } from "react-router-dom";
 // import RouterEx from "./RouterEx";
 import ShopPage from "./pages/shop/shop";
+import Header from "./components/header/header";
 
 /* function HatsPage() {
   return <h1> hats page </h1>;
@@ -12,6 +13,7 @@ import ShopPage from "./pages/shop/shop";
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/shop" component={ShopPage} />
