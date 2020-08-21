88c14fab9bc8bbc913c38a93329c1818512725f8
ventoji <vento.ji@gmail.com>
2020-08-21 14:09:28 +0200

    fixing auth firebase bugs

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Route, Switch } from "react-router-dom";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
 import SignInAndUpPage from "./components/sign-in-and-up/sign-in-and-up";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 /* function HatsPage() {
   return <h1> hats page </h1>;
@@ -24,8 +24,27 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    //  createUserProfileDocument(user)
+     // this.setState({currentUser: user})
+
+     if(userAuth){
+       const userRef =  await createUserProfileDocument(userAuth);
+       userRef.onSnapshot(snapShot => {
+      //   console.log(snapShot.data());
+         this.setState({
+           currentUser: {
+             id: snapShot.id,
+             ...snapShot.data()
+           }
+         }, ()=> {
+          console.log(this.state);
+         })
+       });
+       
+     }else{
+      this.setState({currentUser: userAuth})
+     }
     })
   }
 
