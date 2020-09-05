89ec88009d0931ac434e2ba80701db75005d29f0
ventoji <vento.ji@gmail.com>
2020-09-05 11:22:46 +0200

    adding async actions with redux-thunk

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,39 @@
 import ShopActionsTypes from './shop.types';
+import { firestore, convertCollectionsSnapshoptToMap } from '../../firebase/firebase.utils';
 
-export const updateCollections = (collectionsMap) => ({
+
+export const fetchCollectionsStart = () => ({
+    type: ShopActionsTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSucces = collectionsMap => ({
+    type: ShopActionsTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap
+})
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionsTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+})
+
+export const fetchCollectionsStartAsync = () => {
+ return dispatch => {
+    const collectionRef = firestore.collection('collections');
+      dispatch(fetchCollectionsStart());
+
+       collectionRef.get().then(
+        snapshot => {
+           const collectionsMap = convertCollectionsSnapshoptToMap(snapshot);
+          dispatch(fetchCollectionsSucces(collectionsMap));
+           // updateCollections(collectionMap)
+          // this.setState({loading: false});
+         }).catch(
+             error => dispatch(fetchCollectionsFailure(error.message))
+         ) 
+ }
+};
+
+/* export const updateCollections = (collectionsMap) => ({
     type: ShopActionsTypes.UPDATE_COLLECTIONS,
     payload: collectionsMap
-});
+}); */
--- src/redux/shop/shop.reducer.js
+++ src/redux/shop/shop.reducer.js
@@ -1,14 +1,35 @@
 // import SHOP_DATA from './shop.data';
 
 import ShopActionsTypes from './shop.types';
+import { truncate } from 'lodash';
 
 
 const INITIAL_STATE = {
-  collections: null //SHOP_DATA
+  collections: null, //SHOP_DATA
+  isFetching: false,
+  errorMessage: undefined
 };
 
 const shopReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
+    case ShopActionsTypes.FETCH_COLLECTIONS_START:
+      return {
+        ...state,
+        isFetching: truncate
+      }
+    case ShopActionsTypes.FETCH_COLLECTIONS_SUCCESS:
+      return {
+        ...state,
+        isFetching: false,
+        collections: action.payload
+      }
+    case ShopActionsTypes.FETCH_COLLECTIONS_FAILURE:
+      return {
+        ...state,
+        isFetching: false,
+        errorMessage: action.payload
+      }
+    
     case ShopActionsTypes.UPDATE_COLLECTIONS:
       return {
         ...state,
